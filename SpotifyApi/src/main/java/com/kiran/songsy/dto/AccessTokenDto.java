c872ecfe6a769c39ed0485d77f7db001425f227a
Kiranmai Chigarakula <kiranmaic14@gmail.com>
2025-03-04 17:31:28 -0500

    Updated Token DTO class

--- SpotifyApi/src/main/java/com/kiran/songsy/dto/AccessTokenDto.java
+++ SpotifyApi/src/main/java/com/kiran/songsy/dto/AccessTokenDto.java
@@ -9,9 +9,6 @@ import lombok.NoArgsConstructor;
 public class AccessTokenDto {
 
 	private String access_token;
-	private String token_type;
-	private int expires_in;
-	private String refresh_token;
 
 	public String getAccess_token() {
 		return access_token;
@@ -19,30 +16,6 @@ public class AccessTokenDto {
 
 	public void setAccess_token(String access_token) {
 		this.access_token = access_token;
-	}
-
-	public String getToken_type() {
-		return token_type;
-	}
-
-	public void setToken_type(String token_type) {
-		this.token_type = token_type;
-	}
-
-	public int getExpires_in() {
-		return expires_in;
-	}
-
-	public void setExpires_in(int expires_in) {
-		this.expires_in = expires_in;
-	}
-
-	public String getRefresh_token() {
-		return refresh_token;
-	}
-
-	public void setRefresh_token(String refresh_token) {
-		this.refresh_token = refresh_token;
 	}
 
 }
